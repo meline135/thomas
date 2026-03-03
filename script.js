c8a68f582f319a14340b46e54f7f1bd02c92625b
Méline <meline.moingeon@gmail.com>
2026-03-04 00:16:40 +1300

    Commit all files

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+  Fichier JavaScript - Le Moteur (Le Cerveau)
+  C'est ici qu'on va dire au site quoi faire quand on clique sur les boutons ! 🧠
+*/
+
+// On commence par vérifier que le fichier est bien relié (ça s'affichera dans la console du navigateur)
+console.log("Le moteur est démarré, prêt pour le roadtrip !");
